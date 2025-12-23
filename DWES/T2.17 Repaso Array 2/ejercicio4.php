720abdb800eadeba4987b18c05668849065912a9
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2025-12-23 20:43:36 +0100

    DWES Repaso Array Ej 4 terminado

--- DWES/T2.17 Repaso Array 2/ejercicio4.php
+++ DWES/T2.17 Repaso Array 2/ejercicio4.php
@@ -1,3 +1,18 @@
+<!-- ### Ejercicio 4: Obtener IDs de clientes
+**Función PHP:** `array_keys`
+**Función a implementar:** `obtenerIdsClientes(array $clientes): array`
+
+**Frontend:**
+- Formulario con solo botón "Ejecutar"
+
+**Resultado a mostrar:**
+- Lista de todos los IDs de clientes -->
+
+<?php
+include_once ('funciones.php'); 
+include_once ('datos.php');
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,7 +26,31 @@
         <button type="submit" name="submit">Ejecutar</button>
     </form>
 
-    <!-- TODO: Mostrar resultado cuando se envie el formulario -->
-    <!-- Debe mostrar lista de todos los IDs de clientes -->
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST"):
+
+                if (isset($_POST["submit"])):
+
+                    $ids = obtenerIdsClientes($clientes);
+        ?>
+                <table border="1" cellpadding="5">
+                    <tr>
+                        <td>Id</td><td>Nombre</td>
+                    </tr>
+
+                    <?php
+                    foreach ($ids as $id):
+                    ?>
+                
+                    <tr>
+                        <td><?= $id ?></td>
+                        <td><?= $clientes[$id]['datos']["nombre"] ?></td>
+                    </tr>
+
+                    <?php endforeach ?>
+                    <?php endif ?>
+                    <?php endif ?>
+
+        </table>
 </body>
 </html>
