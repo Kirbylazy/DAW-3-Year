5e78ba9e6edda58004a57f69fbbfe73cec7c3a6a
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2025-12-24 19:04:57 +0100

    DWES Repaso Arrays ej 7 terminado

--- DWES/T2.17 Repaso Array 2/ejercicio7.php
+++ DWES/T2.17 Repaso Array 2/ejercicio7.php
@@ -1,3 +1,22 @@
+<!-- ### Ejercicio 7: Idiomas por nivel de un cliente
+**Funciones PHP:** `foreach` + condicionales
+**Función a implementar:** `obtenerIdiomasPorNivel(array $clientes, string $idCliente, string $nivel): array`
+
+**Frontend:**
+- Formulario con:
+  - Select de clientes (generado dinámicamente)
+  - Input text para nivel (ej: nativo, C1, B2, A2)
+- Botón "Ejecutar"
+
+**Resultado a mostrar:**
+- Lista de idiomas del cliente con ese nivel
+- Tabla con todos los idiomas del cliente para contexto -->
+
+<?php
+include_once ('funciones.php'); 
+include_once ('datos.php');
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -10,8 +29,10 @@
     <form method="POST">
         <label for="idCliente">ID Cliente:</label>
         <select name="idCliente" id="idCliente" required>
-            <option value="">-- Selecciona un cliente --</option>
-            <!-- TODO: Generar opciones dinamicamente con obtenerIdsClientesAux() -->
+            <?php $ids = obtenerIdsClientes($clientes);
+                    foreach ($ids as $id):?>
+            <option value="<?= $id ?>"><?= $id ?></option>
+            <?php endforeach ?>
         </select>
         <br><br>
         <label for="nivel">Nivel:</label>
@@ -19,9 +40,32 @@
         <br><br>
         <button type="submit" name="submit">Ejecutar</button>
     </form>
+    <br><br>
+    
+        <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST"):
 
-    <!-- TODO: Mostrar resultado cuando se envie el formulario -->
-    <!-- Debe mostrar lista de idiomas del cliente con ese nivel -->
-    <!-- Debe mostrar tabla con todos los idiomas del cliente para contexto -->
+                if (isset($_POST["submit"])):?>
+                <table border="1" cellpadding="5">
+                    <tr>
+                        <th>Cliente</th><th>Idiomas por NV</th><th>Idiomas</th>
+                    </tr>
+                    <?php 
+                    $idCliente = $_POST['idCliente'];
+                    $nivel = $_POST['nivel'];
+                    $idiomasNv = obtenerIdiomasPorNivel($clientes, $idCliente, $nivel); 
+                    $idiomas = [];
+                    foreach ($clientes[$idCliente]['idiomas'] as $idioma => $nivel) {
+                        $idiomas[] = "$idioma ($nivel)";
+                    }?>
+
+                    <tr>
+                        <td><?= $idCliente ?></td>
+                        <td><?= implode(', ', $idiomasNv) ?> </td>
+                        <td><?= implode(', ', $idiomas) ?></td>
+                    </tr>
+        </table>
+    <?php endif ?> 
+    <?php endif ?> 
 </body>
 </html>
