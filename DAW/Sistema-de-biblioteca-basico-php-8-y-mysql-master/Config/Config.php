b4c43803e69f2b4a809d828c85823f5108748a8a
Dario Aguilar <kirbylazy@gmail.com>
2026-02-06 19:02:06 +0100

    inclusion de carpeta compartida con VM y nuevo proyecto de DAW

---
+++ DAW/Sistema-de-biblioteca-basico-php-8-y-mysql-master/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/php/DAW/Sistema-de-biblioteca-basico-php-8-y-mysql-master/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "biblioteca_mvc";
+const charset = "charset=utf8";
+?>
