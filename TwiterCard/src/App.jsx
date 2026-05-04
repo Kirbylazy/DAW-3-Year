379815075708eed59489d2c7e02d0f98fa13bf06
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2026-05-04 20:45:20 +0200

    TwiterCard Acabado

--- TwiterCard/src/App.jsx
+++ TwiterCard/src/App.jsx
@@ -1,24 +1,11 @@
-import avatar from './avatar/avatar.jpg'
+import { TwiterFollowCard } from "./TwiterFollowCard";
 
 export function App () {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img 
-                className='tw-followCard-avatar'
-                alt='El avatar de midudev' 
-                src={avatar} alt="Avatar"/>
-                <div className='tw-followCard-info'>
-                    <strong>Dario Aguiar Rodriguez</strong>
-                    <span className='tw-followCard-infoUserName'>@dario</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <>
+        <TwiterFollowCard isFollowing userName='dario' name='Dario Aguilar Rodriguez'/>
+        <TwiterFollowCard userName='rosa' name='Rosa Luque Rodriguez'/>
+        <TwiterFollowCard userName='manolo' name='Manolo Perez Benitez'/>
+        </>
     )
 }
